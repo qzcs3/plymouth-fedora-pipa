50d30d77f3adfb49c82743c3d3d1de2d2e8e5830
qzcs3 <115298989+qzcs3@users.noreply.github.com>
2025-11-19 04:15:53 +0400

    make it more clean script

--- setupPlymouth.sh
+++ setupPlymouth.sh
@@ -7,15 +7,28 @@ mkdir ~/pipa-plymouth-files
 curl -L -o ~/pipa-plymouth-files/charge-new.zip "https://github.com/qzcs3/plymouth-fedora-pipa/raw/main/src/charge-new.zip"
 
 unzip ~/pipa-plymouth-files/charge-new.zip -d ~/pipa-plymouth-files/
+echo ""
+echo "Done"
+echo ""
 
 echo "make it landscape"
 
 sudo rm -rf /usr/share/plymouth/themes/charge/*
 
 sudo cp -r ~/pipa-plymouth-files/charge/* /usr/share/plymouth/themes/charge/
+echo ""
+echo "Done"
+echo ""
 
-echo "enable boot animation"
+echo "enabling boot animation"
 
 sudo plymouth-set-default-theme charge -R
+echo ""
+echo "done"
+echo ""
+echo "deleting dependencies"
 
+rm -rf ~/pipa-plymouth-files
+
+echo ""
 echo "done"
