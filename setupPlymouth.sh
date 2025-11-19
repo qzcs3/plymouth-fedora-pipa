f953cbe03b6ae23a32066f70429d15ed54bc3212
qzcs3 <115298989+qzcs3@users.noreply.github.com>
2025-11-19 04:05:05 +0400

    added setup file

---
+++ setupPlymouth.sh
@@ -0,0 +1,21 @@
+echo "installing required packages"
+
+sudo dnf install plymouth plymouth-plugin-script plymouth-theme-charge unzip
+
+mkdir ~/pipa-plymouth-files
+
+curl -L -o ~/pipa-plymouth-files/charge-new.zip "https://github.com/qzcs3/plymouth-fedora-pipa/raw/main/src/charge-new.zip"
+
+unzip ~/pipa-plymouth-files/charge-new.zip -d ~/pipa-plymouth-files/
+
+echo "make it landscape"
+
+sudo rm -rf /usr/share/plymouth/themes/charge/*
+
+sudo cp -r ~/pipa-plymouth-files/charge/* /usr/share/plymouth/themes/charge/
+
+echo "enable boot animation"
+
+sudo plymouth-set-default-theme charge -R
+
+echo "done"
